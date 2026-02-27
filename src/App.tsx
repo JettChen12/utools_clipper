4e6eaa38bc1b93d40becef30a61552edcf1bdba1
jett <jett@ztgame.com>
2026-02-27 11:51:02 +0800

    feat: 优化光标

--- src/App.tsx
+++ src/App.tsx
@@ -493,6 +493,10 @@ function App() {
                 className="w-full h-full px-3 py-2 bg-gray-50 border border-gray-200 rounded-lg text-sm focus:bg-white focus:border-indigo-500 focus:ring-2 focus:ring-indigo-500/20 outline-none transition-all resize-none"
                 placeholder={t('task.add_placeholder')}
                 autoFocus
+                onFocus={(e) => {
+                  const len = e.target.value.length;
+                  e.target.setSelectionRange(len, len);
+                }}
               />
             </div>
             
